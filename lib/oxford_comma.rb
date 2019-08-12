6691561b4f30ec099f4f497126ec63914c0dacd3
Michael Lynch <michael@michaelrlynch.com>
2019-08-12 03:23:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,15 @@
 def oxford_comma(array)
-  array.join(", ")
-  array.join(" and ")
-  array[-1] = "and #{array[-1]}"
-    array.join(', ')
+  if array.length == 1
+    array.to_s
+  elsif array.length == 2
+    array.join(" and ")
+  end
 end
+
+
+
+
+array.join(", ")
+array.join(" and ")
+array[-1] = "and #{array[-1]}"
+  array.join(', ')
